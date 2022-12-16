ca793ad05f8f199e17fe47131bd40a85e1ff2fcb
Lekhnath <54575651+lekhnath950@users.noreply.github.com>
2022-12-16 12:19:54 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,6 @@ if(process.env.NODE_ENV !== "production") {
 if(process.env.NODE_ENV =='production') {
     const path = require('path')
 
-    // app.get('/',(req,res)=> {
-    //     app.use(express.static(path.resolve(__dirname,'frontend','build')))
-    //     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    // })
-
     app.use(express.static(path.join(__dirname, "./frontend/build")));
     app.get("*",function(_,res) {
         res.sendFile(
